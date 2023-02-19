eab50b56e5f05399dff19f7fd96171908335a9f1
takutsu <t.akutsu@student.tudelft.nl>
2023-02-20 00:12:35 +0100

    シード付き乱数ジェネレータ

--- logger.js
+++ logger.js
@@ -2,13 +2,13 @@
 var log4js = require('log4js');
 log4js.configure({
     appenders: {
-      logFile: { type: 'file', filename: 'error.log' },
+      logFile: { type: 'file', filename: './log/operation.log' },
       out: { type: 'stdout'},
     },
     categories: {
       //default: { appenders: [ 'logFile' ], level: 'error' }
       default: { appenders: [ 'out' ], level: 'trace' },
-      errLog: { appenders: ['logFile'], level: 'error'}
+      errLog: { appenders: ['logFile'], level: 'info'}
     }
   });
 const logger = log4js.getLogger();
